f593605044ac357ead97ebd396626effc27927dd
Wouter Dullaert <wouter.dullaert@tenfoce.com>
2018-02-02 17:06:36 +0100

    Fixed ttl template

--- rdf.go
+++ rdf.go
@@ -17,7 +17,7 @@ func getTTLTemplate() *template.Template {
 	tmpl := "<http://www.specialprivacy.eu/log/{{randomUUID}}><http://www.w3.org/1999/02/22-rdf-syntax-ns#type><http://www.specialprivacy.eu/vocabs/logs#log>;" +
 		"<http://www.specialprivacy.eu/langs/usage-policy#hasPurpose><http://www.specialprivacy.eu/vocabs/purposes#{{.Purpose}}>;" +
 		"<http://www.specialprivacy.eu/langs/usage-policy#hasStorage><http://www.specialprivacy.eu/vocabs/locations#{{.Location}}>;" +
-		"<http://www.specialprivacy.eu/langs/usage-policy#hasDataSubject><http://www.example.com/users/{{.UserId}}>;" +
+		"<http://www.specialprivacy.eu/langs/usage-policy#hasDataSubject><http://www.example.com/users/{{.UserID}}>;" +
 		"<http://www.specialprivacy.eu/langs/usage-policy#hasProcessing><http://www.specialprivacy.eu/vocabs/processing#{{.Process}}>;" +
 		"{{range .Attributes}}<http://www.specialprivacy.eu/langs/usage-policy#hasData><http://www.specialprivacy.eu/vocabs/data#{{.}}>;{{end}}" +
 		"<http://purl.org/dc/terms/created>\"{{toISOTime .Timestamp}}\"^^<http://www.w3.org/2001/XMLSchema#dateTime>."
