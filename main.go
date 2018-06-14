f370f7c6b529b78bf4f1b8b974e2cd9890964a88
Wouter Dullaert <wouter.dullaert@tenfoce.com>
2018-06-14 14:01:24 +0200

    Bump version number in the code

--- main.go
+++ main.go
@@ -111,7 +111,7 @@ func main() {
 	app.Usage = "Create a stream of pseudo random events in the Special format"
 	app.ArgsUsage = " "
 	app.EnableBashCompletion = true
-	app.Version = "1.0.0"
+	app.Version = "1.5.0"
 	app.Authors = []cli.Author{
 		{
 			Name:  "Wouter Dullaert",
